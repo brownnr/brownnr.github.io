988bab15abd7890de65c8546da3eb328f9965267
Nick Brown <brown.nicholas5@gmail.com>
2016-05-19 21:09:07 -0500

    Test 1

--- js/calls.js
+++ js/calls.js
@@ -5,18 +5,20 @@ document.addEventListener('DOMContentLoaded', getStations);
 function getStations(){
   //add listener for click to submit data
   document.getElementById("submit").addEventListener('click', function(event){
-	  var req = new XMLHttpRequest();
+	  //stuff for alt. fuel stations
+	  var fReq = new XMLHttpRequest();
 	  var loc = document.getElementById("location").value;
 	  var rad = document.getElementById("radius").value;
 		
-	  req.open("GET", "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?
+	  fReq.open("GET", "https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?
 		api_key=" + k + "&location=" + loc + "&radius=" + rad + "&status=E&access=
 		public&limit=2", true);
-	  req.addEventListener('load', function(){
+	  fReq.addEventListener('load', function(){
 		
-		  if(req.status >= 200 && req.status < 400){
-			var res = JSON.parse(req.responseText);
-			var stations = res.fuel_stations;
+		  if(fReq.status >= 200 && fReq.status < 400){
+			var fRes = JSON.parse(fReq.responseText);
+			var stations = fRes.fuel_stations;
+			var fuelForm = document.getElementById("altFuel");
 			var max;
 			
 			if(stations.length - 1 < 10){
@@ -26,34 +28,37 @@ function getStations(){
 			}
 			
 			for(x = 0; x < max; x++){
-			  var station = res.fuel_stations[x];
+			  var station = fRes.fuel_stations[x];
 			  var p = document.createElement("p");
 					
 			  p.innerHTML = "&lt;b&gt" + station.station_name + "&lt;/b&gt&lt;br&gt" + station.street_address
 				+ "&lt;br&gt" + station.city + ", " + station.state + " " + station.zip + "&lt;br&gt" + 
 				station.station_phone + "&lt;br&gt";
 
-			  document.body.appendChild(p);
+			  fuelForm.appendChild(p);
 			}
 		  } else {
 			var head = document.createElement("h2");
 			var p = document.createElement("p");
 			
 			head.innerHTML = "Error in the network request:"
-			p.innerHTML = req.statusText;
+			p.innerHTML = fReq.statusText;
 			
 			document.body.appendChild(head);
 			document.body.appendChild(p);
 		  }
 	  });
 		
-	  req.send(null);
+	  fReq.send(null);
 	  
 	  
 	  
 	  
 	  
-	  
+	  var reveal = document.getElementsByClassName("col-md-5");
+	  for(var x = 0; x < reveal.length; x++){
+		  reveal[x].style.visibility = "visible";
+	  }
 	  event.preventDefault();
 	}
 }
