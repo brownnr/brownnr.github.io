1e21c4abb7e46f458c6401894c6e50962a3c6f65
Nick Brown <para-programmer@users.noreply.github.com>
2017-12-12 13:33:50 -0600

    Still tinkering with shuffling

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
 	var text = "";
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}`~;':|\\\",./<>?";
 
-	for (var i = 0; i < 5; i++)
+	for (var i = 0; i < 24; i++)
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	
+	$('#greeting').html(text);
+	
 	var times = Math.floor(Math.random() * (12 - 3) + 3);
-	var target = $('blockquote').shuffleText(text, {
+	var target = $('#greeting').shuffleText("Welcome to my portfolio!", {
 		frames: 60,
 		maxSpeed: 1500,
 		amount: times,
