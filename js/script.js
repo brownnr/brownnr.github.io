d0c67a089d284ec2245d6acd4cfa5c3513ac9328
Nick Brown <para-programmer@users.noreply.github.com>
2017-12-18 10:31:52 -0600

    Updating greeting

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(window).bind("load", function() {
 	var times = Math.floor(Math.random() * (12 - 3) + 3);
 	
-	var target = $('#greeting').showGreeting("Hello", {
+	var target = $('#greeting').showGreeting("Welcome", {
 		frames: 1000,
 		amount: times,
 		complete: null
