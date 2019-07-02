f93b5b7a66373bb14a0fca6816515c237908a7eb
Nick <brown.nicholas5@gmail.com>
2019-07-02 11:59:27 -0500

    Updates

---
+++ precache-manifest.77688b251d23edc9ec0b53e480be9139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c701a21d7e225f242654325dd5f2bd4f",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "53f329e86c752127a89f",
+    "url": "/myapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/myapp/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "53f329e86c752127a89f",
+    "url": "/myapp/static/js/main.f226d567.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
